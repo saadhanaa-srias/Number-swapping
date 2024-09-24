449d3bbe5c347379182252c02f07ba2ac4d4b6d1
saadhanaa-srias <saadhanaasrias@gmail.com>
2024-09-24 18:41:24 +0530

    L1

---
+++ main.py
@@ -0,0 +1,17 @@
+x=int(input("enter your first number "))
+y=int(input("enter your second number "))
+z=int(input("enter your third number "))
+sum=x+y+z
+print("when added:",sum)
+sub=x-y-z
+print("when minused:",sub)
+mul=x*y*z
+print("when multiplied:",mul)
+div=x/y
+print("when divided:",div)
+fldiv=x//y//z
+print("when floor divided:",fldiv)
+mod=z%y
+print("when modulo is used:",mod)
+exp=z**x
+print("total exponent:",exp)
